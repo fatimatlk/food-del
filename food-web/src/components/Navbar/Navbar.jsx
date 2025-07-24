bbafdea3a6543a19419ecd23ea4a5748c0b06044
fatimatlk <fatimatlk80@gmail.com>
2025-07-24 04:00:21 -0700

    build

---
+++ food-web/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={} alt="" className="logo" />
+    </div>
+  )
+}
+
+export default Navbar
